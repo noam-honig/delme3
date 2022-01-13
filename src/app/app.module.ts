ed671317d0320d423b97d75dfa04a45f1500d21a
Noam Honig <noam.honig@gmail.com>
2022-01-13 12:01:36 +0200

    Added toolbar with two links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TodoComponent } from './todo/todo.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    MatToolbarModule,
+    MatButtonModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
